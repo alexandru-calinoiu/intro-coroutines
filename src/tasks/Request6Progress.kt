bc43e1e702cd225d68c753768c04adbd0f3c5f76
calin <calinoiu.alexandru@agilefreaks.com>
2019-09-13 16:19:03 +0300

    Fix tests

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,19 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    val result = repos.foldRight(emptyList<User>(), { repo, acc ->
+        val list = (service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList() + acc).aggregate()
+        updateResults(list, false)
+        list
+    })
+
+    updateResults(result, true)
 }
--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -9,8 +9,30 @@ suspend fun loadContributorsChannels(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
-) {
-    coroutineScope {
-        TODO()
+) = coroutineScope {
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    val channel = Channel<List<User>>()
+    repos.forEach { repo ->
+        launch {
+            val users = service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+            channel.send(users)
+        }
+    }
+
+    launch {
+        var result = mutableListOf<User>()
+        repeat(repos.count()) {
+            val list = channel.receive()
+            result = (result + list).aggregate().toMutableList()
+            updateResults(result, it == repos.lastIndex)
+        }
     }
 }
+
